aa90ff13d6bd1386bfac3da0af3c0b4d0e7cdafa
puja kunche <kunchepuja1994@gmail.com>
2024-07-24 03:46:09 +0000

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import Controller.SocialMediaController;
+import Util.ConnectionUtil;
 import io.javalin.Javalin;
 
 /**
@@ -6,9 +12,40 @@ import io.javalin.Javalin;
  * affect your program in any way and you may write whatever code you like here.
  */
 public class Main {
+   
+
+   
     public static void main(String[] args) {
         SocialMediaController controller = new SocialMediaController();
         Javalin app = controller.startAPI();
-        app.start(8080);
+        app.start(8080);    
+        databaseSetup();
+
+    }
+
+    public static void databaseSetup(){
+        try {
+            Connection conn = ConnectionUtil.getConnection();
+            PreparedStatement ps1 = conn.prepareStatement("drop table if exists message");
+            ps1.executeUpdate();
+            PreparedStatement ps1b = conn.prepareStatement("drop table if exists account");
+            ps1b.executeUpdate();
+            PreparedStatement ps3 = conn.prepareStatement("create table account (account_id int primary key auto_increment, username varchar(255) unique, password varchar(255));"
+            );
+            ps3.executeUpdate();
+            PreparedStatement ps4 = conn.prepareStatement(
+                "create table message (" +
+                    "message_id int primary key auto_increment," + 
+                    "posted_by int," +
+                    "message_text varchar(255)," +
+                    "time_posted_epoch bigint," +
+                    "foreign key (posted_by) references  account(account_id)" +
+                ");"
+            );
+            ps4.executeUpdate();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
